47a37ddaee512eaf96ac1a7a81500df6e8b3a8cd
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-30 19:03:23 +0900

    Create singleton.h

---
+++ include/singleton.h
@@ -0,0 +1,14 @@
+class singleton
+{
+private:
+    singleton();
+    singleton(const singleton &) = delete; 
+    singleton &operator=(const singleton &) = delete; 
+    
+    static std::unique_ptr<singleton> _Myinstance;
+    static std::once_flag _Myonce_flag;
+public:
+    static singleton& instance();
+
+};
+
