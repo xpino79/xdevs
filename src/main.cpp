4cf35b9a59efaf81b533c43e9239d240673cebe1
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-30 21:19:43 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,21 @@
 #include <map>
 #include <list>
 
+// MISRA_CPP_03_09_02 기본 숫자 타입 대신 크기와 부호를 나타내는 typedef를 사용해야 함
 #include "../include/typedefs.h"
 
-// MISRA_CPP_2008 
 std::int32_t main(std::int32_t argc, char_t *argv[])
 {
       std::int32_t k = 1;
       std::map<std::int32_t, std::int32_t> _Map;
       _Map.insert(std::make_pair(key, 0));
 
-      // insert return
+      // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지 
       auto _Pair = _Map.insert(std::make_pair(key, 0));
       if (!_Pair.second) {
             // error
       }
-      
-      // erase return
+      // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지 
       if (0==_Map.erase( k )) {
             // error
       }
