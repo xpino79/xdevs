736542e8e609d12983829cca55212a7f016ff0d4
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-30 21:36:25 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,30 @@
 
 std::int32_t main(std::int32_t argc, char_t *argv[])
 {
-      std::int32_t k = 1;
+      std::int32_t _Key = 1;
       std::map<std::int32_t, std::int32_t> _Map;
-      _Map.insert(std::make_pair(key, 0));
+      std::list<std::int32_t> _List;
+      
+      _Map.insert(std::make_pair(_Key, 0));
+      _List.push_back(_Key);
 
-      // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지 
-      auto _Pair = _Map.insert(std::make_pair(key, 0));
-      if (!_Pair.second) {
-            // error
+      { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
+            auto _Pair = _Map.insert(std::make_pair(key, 0));
+            if (!_Pair.second) {
+                  // error
+            }
+            if (0==_Map.erase( _Key )) {
+                  // error
+            }
       }
-      // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지 
-      if (0==_Map.erase( k )) {
-            // error
+      { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
+            auto _Iter = std::find(_List.begin(), _List.end(), _Key);
+            if ( _Iter != _List.end() )
+            {
+                  _Iter = _List.erase( _Iter );
+                  // _Iter 변수 >>> MISRA_CPP_00_01_06 변수 값 할당 후에 한 번도 사용하지 않는 프로세스 금지 
+                  // 해결방안 ?
+            }
       }
       
       return 1;
