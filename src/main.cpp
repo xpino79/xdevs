b75498cea50097061bc9117e06ee94a95146f233
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-30 20:44:46 +0900

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <map>
+#include <list>
+
+#include "../include/typedefs.h"
+
+// MISRA_CPP_2008 
+std::int32_t main(std::int32_t argc, char_t *argv[])
+{
+
+      return 1;
+}
