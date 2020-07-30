fefc16a73180bf6221f45f72e3667580e4687786
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-30 20:57:08 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,20 @@
 // MISRA_CPP_2008 
 std::int32_t main(std::int32_t argc, char_t *argv[])
 {
+      std::int32_t k = 1;
+      std::map<std::int32_t, std::int32_t> _Map;
+      _Map.insert(std::make_pair(key, 0));
 
+      // insert return
+      auto _Pair = _Map.insert(std::make_pair(key, 0));
+      if (!_Pair.second) {
+            // error
+      }
+      
+      // erase return
+      if (0==_Map.erase( k )) {
+            // error
+      }
+      
       return 1;
 }
