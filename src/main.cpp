92c36d1c6cf7cc83b37bfd7049091fc8069da925
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-31 14:31:51 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,42 +8,44 @@
 
 std::int32_t main(std::int32_t argc, char_t *argv[])
 {
-      { // MISRA_CPP_18_04_01 동적 힙 메모리 할당은 사용하면 안됨 
-            
-      }
-      { // MISRA_CPP_05_02_03 상위(base) 클래스를 하위(derived)클래스로 변환하는 것은 다형(polymorphic) 타입 간에 이루어져서는 안됨
-            for (auto &_Elem : api_devs::xmanager::instance().xobjects())
-            {
-                api_devs::xair *_Xair = dynamic_cast<api_devs::xair *>(_Elem.second.get());
-                api_devs::xsea *_Xsea = dynamic_cast<api_devs::xsea *>(_Elem.second.get());
-                // down casting, cross casting 해결방안 ?
-            }
-      }
-      { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
-            std::int32_t _Key = 1;
-            std::map<std::int32_t, std::int32_t> _Map;
-            _Map.insert(std::make_pair(_Key, 0));
-            auto _Pair = _Map.insert(std::make_pair(key, 0));
-            if (!_Pair.second) {
-                  // error
-            }
-            if (0==_Map.erase( _Key )) {
-                  // error
-            }
-      }
-      { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
-            std::int32_t _Key = 1;
-            std::list<std::int32_t> _List;
-            _List.push_back(_Key);
-            auto _Iter = std::find(_List.begin(), _List.end(), _Key);
-            if ( _Iter != _List.end() )
-            {
-                  _Iter = _List.erase( _Iter );
-                  // _Iter 변수 >>> MISRA_CPP_00_01_06 변수 값 할당 후에 한 번도 사용하지 않는 프로세스 금지 
-                  // 해결방안 ?
-            }
-      }
-
-      
-      return 1;
+    { // MISRA_CPP_18_04_01 동적 힙 메모리 할당은 사용하면 안됨 
+        std::int32_t _Key = 1;
+        unique_ptr<api_devs::xair> _Uptr = std::make_unique<api_devs::xair>();
+        _Uptr->set_key(_Key);
+        api_devs::xmanager::instance().insert( std::move(_Uptr) );
+    }
+    { // MISRA_CPP_05_02_03 상위(base) 클래스를 하위(derived)클래스로 변환하는 것은 다형(polymorphic) 타입 간에 이루어져서는 안됨
+        for (auto &_Elem : api_devs::xmanager::instance().xobjects())
+        { 
+            api_devs::xair *_Xair = dynamic_cast<api_devs::xair *>(_Elem.second.get());
+            api_devs::xsea *_Xsea = dynamic_cast<api_devs::xsea *>(_Elem.second.get());
+            // down casting, cross casting 해결방안 ?
+          }
+    }
+    { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
+          std::int32_t _Key = 1;
+          std::map<std::int32_t, std::int32_t> _Map;
+          _Map.insert(std::make_pair(_Key, 0));
+          auto _Pair = _Map.insert(std::make_pair(key, 0));
+          if (!_Pair.second) {
+                // error
+          }
+          if (0==_Map.erase( _Key )) {
+                // error
+          }
+    }
+    { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
+          std::int32_t _Key = 1;
+          std::list<std::int32_t> _List;
+          _List.push_back(_Key);
+          auto _Iter = std::find(_List.begin(), _List.end(), _Key);
+          if ( _Iter != _List.end() )
+          {
+                _Iter = _List.erase( _Iter );
+                // _Iter 변수 >>> MISRA_CPP_00_01_06 변수 값 할당 후에 한 번도 사용하지 않는 프로세스 금지 
+                // 해결방안 ?
+          }
+    }
+  
+    return 1;
 }
