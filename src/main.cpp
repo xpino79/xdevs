e68d9b1c797f1cd46fcde42358d34d0167422961
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-31 11:11:37 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,8 @@ std::int32_t main(std::int32_t argc, char_t *argv[])
       { // MISRA_CPP_05_02_03 상위(base) 클래스를 하위(derived)클래스로 변환하는 것은 다형(polymorphic) 타입 간에 이루어져서는 안됨
             for (auto _Elem : api_devs::xmanager::instance().xobjects())
             {
-                   api_devs::xair *_Xair = dynamic_cast<api_devs::xair *>(_Elem.second.get());
+                api_devs::xair *_Xair = dynamic_cast<api_devs::xair *>(_Elem.second.get());
+                // 해결방안 ?
             }
       }
       { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
