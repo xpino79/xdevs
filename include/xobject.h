7baeba4e46667b026327d258fc17a294bb04f512
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-31 10:42:57 +0900

    Create xobject.h

---
+++ include/xobject.h
@@ -0,0 +1,29 @@
+
+
+class xobject
+{
+public:
+    xobject() = default;
+    virtual ~xobject() = default;
+};
+
+class xair : public xobject
+{
+public:
+    xair() = default;
+    virtual ~xair() = default;
+};
+
+class xsea : public xobject
+{
+public:
+    xsea() = default;
+    virtual ~xsea() = default;
+};
+
+class xground : public xobject
+{
+public:
+    xground() = default;
+    virtual ~xground() = default;
+};
