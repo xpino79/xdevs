6739bbea765ce813fa613d7619e378ca531de3b7
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-07-31 11:18:06 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ std::int32_t main(std::int32_t argc, char_t *argv[])
             for (auto &_Elem : api_devs::xmanager::instance().xobjects())
             {
                 api_devs::xair *_Xair = dynamic_cast<api_devs::xair *>(_Elem.second.get());
+                api_devs::xsea *_Xsea = dynamic_cast<api_devs::xsea *>(_Elem.second.get());
                 // cross casting 해결방안 ?
             }
       }
