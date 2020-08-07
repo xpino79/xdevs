2ff4179d670275533a8eee3a054170161c6240cf
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 12:14:45 +0900

    Update xobject.h

--- include/xobject.h
+++ include/xobject.h
@@ -11,6 +11,10 @@ public:
     virtual ~xobject() = default;
     int32_t key() const { return _Mykey;}
     void set_key(int32_t _Key) { _Mykey = _Key;}
+    
+    // MISRA_CPP_10_03_01 상속 계층을 따라 각 virtual function 정이는 하나씩만 존재해야 함.
+    // MISRA_CPP_10_03_03 재선언되는 base클래스의 함수가 pure virtual일 때만 pure virtual함수로 재선언 할 수 있음. 
+    virtual void refresh() = 0;
 };
 
 class xair : public xobject
@@ -18,6 +22,8 @@ class xair : public xobject
 public:
     xair() = default;
     virtual ~xair() = default;
+    
+    virtual void refresh() final {} 
 };
 
 class xsea : public xobject
@@ -25,6 +31,8 @@ class xsea : public xobject
 public:
     xsea() = default;
     virtual ~xsea() = default;
+    
+    virtual void refresh() final {} 
 };
 
 class xground : public xobject
@@ -32,6 +40,8 @@ class xground : public xobject
 public:
     xground() = default;
     virtual ~xground() = default;
+    
+    virtual void refresh() final {} 
 };
 }
 
