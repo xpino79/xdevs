522947279483aba54d617b0b05d4213cb79de2de
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 16:37:27 +0900

    Update xobject.h

--- include/xobject.h
+++ include/xobject.h
@@ -15,6 +15,9 @@ public:
     // MISRA_CPP_10_03_01 상속 계층을 따라 각 virtual function 정이는 하나씩만 존재해야 함.
     // MISRA_CPP_10_03_03 재선언되는 base클래스의 함수가 pure virtual일 때만 pure virtual함수로 재선언 할 수 있음. 
     virtual void refresh() = 0;
+    virtual std::string name() = 0;
+    virtual void set_name(std::string _Val) = 0;
+    
 };
 
 class xground : public xobject
@@ -23,13 +26,29 @@ public:
     xground() = default;
     virtual ~xground() = default;
 };
+    
 class xmaneuver : public xground
 {
+private:
+    std::string _Myname;
 public:
     xmaneuver() = default;
     virtual ~xmaneuver() = default;
     
-    virtual void refresh() final {} 
+    virtual void refresh() final 
+    { 
+        std::cout << this->key() << std::endl;
+        std::cout << this->name() << std::endl;
+    } 
+    virtual std::string name() final 
+    { 
+        return this->_Myname; 
+    }
+    virtual void set_name(const std::string &_Val) final 
+    { 
+        this->_Myname = _Val; 
+    }
+    
 };
 class xsupport : public xground
 {
