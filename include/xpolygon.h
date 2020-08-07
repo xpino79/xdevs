c2e5547ec6b4debfce84302c78838cd4ed2c039e
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 21:48:45 +0900

    Update xpolygon.h

--- include/xpolygon.h
+++ include/xpolygon.h
@@ -1,11 +1,16 @@
-#include "xtypedefs.h"
+#include <memory>
+#include <mutex>
+#include <vector>
+
+#include "xcoordinate.h"
 
 namespace proj_devs
 {
 class xpolygon
 {
 private:
-  
+    std::vector<std::unique_ptr<xcoordinate>> _Myvertex;
+    
 public:
     xpolygon() = default;
     virtual ~xpolygon() = default;
