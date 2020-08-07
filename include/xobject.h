fa123531ac474d5b98907fc58a1674cdd92579fc
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 19:42:56 +0900

    Update xobject.h

--- include/xobject.h
+++ include/xobject.h
@@ -10,8 +10,8 @@ private:
 public:
     xobject() = default;
     virtual ~xobject() = default;
-    int32_t key() const { return _Mykey;}
-    void set_key(int32_t _Key) { _Mykey = _Key;}
+    int32_t key() const;
+    void set_key(int32_t _Key);
     
     // MISRA_CPP_10_03_01 상속 계층을 따라 각 virtual function 정이는 하나씩만 존재해야 함.
     // MISRA_CPP_10_03_03 재선언되는 base클래스의 함수가 pure virtual일 때만 pure virtual함수로 재선언 할 수 있음. 
@@ -36,14 +36,8 @@ public:
     xground() = default;
     virtual ~xground() = default;
     
-    virtual std::string name() const final 
-    { 
-        return this->_Myname; 
-    }
-    virtual void set_name(const std::string &_Val) final 
-    { 
-        this->_Myname = _Val; 
-    }
+    virtual std::string name() const final;
+    virtual void set_name(const std::string &_Val) final;
 };
     
 class xmaneuver : public xground
@@ -55,22 +49,9 @@ public:
     xmaneuver() = default;
     virtual ~xmaneuver() = default;
     
-    virtual void refresh() final 
-    { 
-        std::cout << "xmaneuver::refresh start " << std::endl;
-        std::cout << this->key() << std::endl;
-        std::cout << this->name() << std::endl;
-        std::cout << this->course() << std::endl;
-        std::cout << "xmaneuver::refresh end " << std::endl;
-    }
-    virtual std::int32_t course() const final
-    {
-        return this->_Mycourse; 
-    }
-    virtual void set_course(std::int32_t _Val) final
-    {
-        this->_Mycourse = _Val; 
-    }
+    virtual void refresh() final;
+    virtual std::int32_t course() const final;
+    virtual void set_course(std::int32_t _Val) final;
 };
 class xsupport : public xground
 {
@@ -78,17 +59,8 @@ public:
     xsupport() = default;
     virtual ~xsupport() = default;
     
-    virtual void refresh() final 
-    { 
-        std::cout << "xsupport::refresh start " << std::endl;
-        std::cout << this->key() << std::endl;
-        std::cout << this->name() << std::endl;
-        std::cout << "xsupport::refresh end " << std::endl;
-    }
-    virtual std::int32_t course() const final
-    {
-        return 0; 
-    }
+    virtual void refresh() final;
+    virtual std::int32_t course() const final;
     virtual void set_course(std::int32_t _Val) final default;
 };
     
