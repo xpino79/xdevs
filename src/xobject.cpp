bf54cba960ad8174b914de8c02a66529c6166b8e
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 19:42:59 +0900

    Create xobject.cpp

---
+++ src/xobject.cpp
@@ -0,0 +1,52 @@
+
+#include "../include/xobject.h"
+
+namespace proj_devs
+{
+int32_t xobject::key() const 
+{ 
+    return _Mykey;
+}
+void xobject::set_key(int32_t _Key) 
+{ 
+    _Mykey = _Key;
+}
+
+std::string xground::name() const 
+{ 
+    return this->_Myname; 
+}
+void xground::set_name(const std::string &_Val) 
+{ 
+    this->_Myname = _Val; 
+}
+
+void xmaneuver::refresh() 
+{ 
+    std::cout << "xmaneuver::refresh start " << std::endl;
+    std::cout << this->key() << std::endl;
+    std::cout << this->name() << std::endl;
+    std::cout << this->course() << std::endl;
+    std::cout << "xmaneuver::refresh end " << std::endl;
+}
+std::int32_t xmaneuver::course() const
+{
+    return this->_Mycourse; 
+}
+void xmaneuver::set_course(std::int32_t _Val)
+{
+    this->_Mycourse = _Val; 
+}
+void xsupport::refresh() 
+{ 
+    std::cout << "xsupport::refresh start " << std::endl;
+    std::cout << this->key() << std::endl;
+    std::cout << this->name() << std::endl;
+    std::cout << "xsupport::refresh end " << std::endl;
+}
+std::int32_t xsupport::course() const
+{
+    return 0; 
+}  
+
+}
