1a6e32ed9c724be8863da546e8f002a1e1b915fd
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 21:35:15 +0900

    Create xcoordinate.h

---
+++ include/xcoordinate.h
@@ -0,0 +1,22 @@
+#include "xtypedefs.h"
+
+namespace proj_devs
+{
+class xcoordinate
+{
+private:
+  std::int32_t _Myx;
+  std::int32_t _Myy;
+  
+public:
+    xcoordinate(std::int32_t _x, std::int32_t _y)
+    : _Myx(_x), _Myy(_y)
+    {
+    }
+    ~xcoordinate() = default;
+    
+    std::int32_t x() { return _Myx; }
+    std::int32_t y() { return _Myy; }
+};
+
+} /* namespace proj_devs */
