b86c982648873c72e807c79d35dd672095c68f65
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 22:08:45 +0900

    Update xmulti_polygon.h

--- include/xmulti_polygon.h
+++ include/xmulti_polygon.h
@@ -1,11 +1,13 @@
 #include "xtypedefs.h"
+#include "xpolygon.h"
 
 namespace proj_devs
 {
 class xmulti_polygon
 {
 private:
-  
+    std::vector<std::unique_ptr<xpolygon>> _Mypolygons;
+    
 public:
     xmulti_polygon() = default;
     virtual ~xmulti_polygon() = default;
