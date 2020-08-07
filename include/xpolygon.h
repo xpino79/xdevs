c13cc58aa6cf7d5d96ff1744785ac869bd3611b1
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 22:13:42 +0900

    Update xpolygon.h

--- include/xpolygon.h
+++ include/xpolygon.h
@@ -9,9 +9,11 @@ namespace proj_devs
 class xpolygon
 {
 private:
-    std::vector<std::unique_ptr<xcoordinate>> _Myvertex;
+    std::vector<std::unique_ptr<xcoordinate>> _Mycoordinates;
     
 public:
     xpolygon() = default;
     virtual ~xpolygon() = default;
+    
+    void add(std::int32_t _x, std::int32_t _y)
 };
