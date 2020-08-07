02110e07ec7b4e7d0940fd28aaff8cd9fcf83f7f
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-07 22:24:21 +0900

    Update xpolygon.h

--- include/xpolygon.h
+++ include/xpolygon.h
@@ -18,4 +18,14 @@ public:
     void push_back(std::int32_t _x, std::int32_t _y);
     void push_back(std::unique_ptr<xcoordinate>);
     
+    std::float64_t area();
+    std::float64_t length();
+    
+    bool empty();
+    bool disjoint(std::int32_t _x, std::int32_t _y);
+    bool intersects(std::int32_t _x, std::int32_t _y);
+    bool crosses(std::int32_t _x, std::int32_t _y);
+    bool within(std::int32_t _x, std::int32_t _y);
+    bool contains(std::int32_t _x, std::int32_t _y);
+
 };
