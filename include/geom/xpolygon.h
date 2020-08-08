26bca64a624d8ed4e7743084b16d68e38674af95
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-08 20:06:07 +0900

    Update xpolygon.h

--- include/geom/xpolygon.h
+++ include/geom/xpolygon.h
@@ -63,8 +63,42 @@ public:
     {
     }
 
+    // https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+    // https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html
     bool within(std::int32_t _X, std::int32_t _Y)
     {
+        // If we never cross any lines we're inside.
+        bool _Inside = false;
+	    std::int32_t _Max = this->_Mycoordinates.size();
+         
+	    for (std::int32_t _Num = 0, _Num < _Max; _Num++)
+	    {
+            // _Num is the index of the first vertex, _Next is the next one.
+            std::int32_t _Next = (_Num + 1) % _Max;
+            
+            // The vertices of the edge we are checking.
+            std::int32_t _X0 = this->_Mycoordinates[_Num]->x();
+		    std::int32_t _Y0 = this->_Mycoordinates[_Num]->y();
+		    std::int32_t _X1 = this->_Mycoordinates[_Next]->x();
+		    std::int32_t _Y1 = this->_Mycoordinates[_Next]->y();
+
+            // First check if the line crosses the horizontal line at _Y in either direction.
+            if (((_Y0 <= _Y) && (_Y1 > _Y)) || ((_Y1 <= _Y) && (_Y0 > _Y)))
+            {
+                // If so, get the point where it crosses that line. This is a simple solution
+                // to a linear equation. Note that we can't get a division by zero here -
+                // if _Y1 == _Y0 then the above if be false.
+                std::float64_t _Cross = (_X1 - _X0) * (_Y - _Y0) / (_Y1 - _Y0) + _X0;
+                
+                // Finally check if it crosses to the left of our test point. You could equally
+                // do right and it should give the same result.
+                if (_Cross < _X)
+                {
+                    _Inside = !_Inside;
+                }
+            }
+        } /* for */
+        
     }
  
 };
