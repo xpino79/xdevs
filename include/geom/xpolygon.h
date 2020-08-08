71e601274cbbbc9b98af22535b1e420ee91b9a95
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-08 21:21:15 +0900

    Update xpolygon.h

--- include/geom/xpolygon.h
+++ include/geom/xpolygon.h
@@ -71,7 +71,7 @@ public:
         bool _Inside = false;
         std::int32_t _Max = this->_Mycoordinates.size();
     
-        for (std::int32_t _Num = 0, _Num < _Max; _Num++)
+        for (std::int32_t _Num = 0, _Num < _Max; ++_Num)
         {
             // _Num is the index of the first vertex, _Next is the next one.
             std::int32_t _Next = (_Num + 1) % _Max;
