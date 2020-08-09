16ea0289d1f46118a589e51cabac0b18c351b0e6
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-09 15:01:43 +0900

    Update xtypedefs.h

--- include/xtypedefs.h
+++ include/xtypedefs.h
@@ -2,11 +2,16 @@
  * Copyright 2020. xpino79@gmail.com All rights reserved.
  */
 
-#include <cstdint>
+#ifndef XTYPEDEFS_H_
+#define XTYPEDEFS_H_
 
+#include <cstdint>
 namespace std
 {
 typedef float float32_t;
 typedef double float64_t;
 typedef char char_t;
 }
+ 
+#endif /* XTYPEDEFS_H_ */
+
