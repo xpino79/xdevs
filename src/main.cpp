08924369164b2b9d26446055cd630097e4bdb04a
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-09 15:28:16 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 std::int32_t main(std::int32_t argc, std::char_t *argv[])
 {
     {// POLYGON || MULTIPOLYGON
-        std::unique_ptr<proj_devs::xpolygon> _Puptr = std::make_unique<proj_devs::xpolygon>();
+        std::unique_ptr<my::xpolygon> _Puptr = std::make_unique<my::xpolygon>();
         _Puptr->push_back(0, 0);
         _Puptr->push_back(2, 0);
         _Puptr->push_back(2, 2);
@@ -24,7 +24,7 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
         std::cout << "POLYGON length: " << _Puptr->length() << std::endl;
         std::cout << "POLYGON within: " << _Puptr->within(10, 10) << ", " << _Puptr->within(0, 0) << ", " << _Puptr->within(1, 1) << std::endl;
         
-        std::unique_ptr<proj_devs::xmulti_polygon> _MPuptr = std::make_unique<proj_devs::xmulti_polygon>();
+        std::unique_ptr<my::xmulti_polygon> _MPuptr = std::make_unique<my::xmulti_polygon>();
         _MPuptr->push_back( std::move(_Puptr) );
         std::cout << "MULTIPOLYGON area: " << _MPuptr->area() << std::endl;
         std::cout << "MULTIPOLYGON length: " << _MPuptr->length() << std::endl;
@@ -32,21 +32,21 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
  
     }
     { // MISRA_CPP_18_04_01 동적 힙 메모리 할당은 사용하면 안됨 
-        proj_devs::xobject *_Ptr = nullptr;
-        _Ptr = proj_devs::xmanager::instance().insert( std::make_unique<proj_devs::xmaneuver>() );
+        my::xobject *_Ptr = nullptr;
+        _Ptr = my::xmanager::instance().insert( std::make_unique<my::xmaneuver>() );
         if (nullptr != _Ptr)
         {
             // something
         }
         
-        _Ptr = proj_devs::xmanager::instance().insert( std::make_unique<proj_devs::xsupport>() );
+        _Ptr = my::xmanager::instance().insert( std::make_unique<my::xsupport>() );
         if (nullptr != _Ptr)
         {
             // something
         }
     }
     { // MISRA_CPP_05_02_03 상위(base) 클래스를 하위(derived)클래스로 변환하는 것은 다형(polymorphic) 타입 간에 이루어져서는 안됨
-        for (auto &_Elem : proj_devs::xmanager::instance().xobjects())
+        for (auto &_Elem : my::xmanager::instance().xobjects())
         { 
             // down casting, cross casting 해결방안 ?
             // proj_devs::xground *_Ptr = std::dynamic_cast<proj_devs::xground *>(_Elem.second.get());
@@ -54,7 +54,7 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
 
             // MISRA_CPP_10_03_01 상속 계층을 따라 각 virtual function 정이는 하나씩만 존재해야 함
             // 하위(derived)클래스의 함수를 상위(base)클래스 pure virtual 로 만든다.
-            proj_devs::xobject *_Ptr = _Elem.second.get();
+            my::xobject *_Ptr = _Elem.second.get();
             _Ptr->refresh();
         }
     }
