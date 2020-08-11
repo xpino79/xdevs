c39d4e95ed3bce30da73a91f81fea48e309d9719
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-11 13:15:41 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -60,9 +60,10 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
             {
                 _Ptr->refresh();
             }
-            std::cout << "std::shared_ptr::#1 " << _Elem.second.use_count() << std::endl;
         }
-
+        my::xmanager::instance().erase( 1 );
+        std::cout << "erase >>>>>>" << std::endl;
+        
     }
     { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
           std::int32_t _Key = 1;
