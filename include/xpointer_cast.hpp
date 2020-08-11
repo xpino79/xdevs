f702dc76e6e9a58fdf97833ed1e8da91265bb9c7
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-11 21:48:06 +0900

    Update xpointer_cast.hpp

--- include/xpointer_cast.hpp
+++ include/xpointer_cast.hpp
@@ -1,1 +1,11 @@
-x
+namespace my {
+
+template<class T, class U>
+inline T* static_pointer_cast(U *ptr)
+{ return static_cast<T*>(ptr); }
+
+template<class T, class U>
+inline T* dynamic_pointer_cast(U *ptr)
+{ return dynamic_cast<T*>(ptr); }
+
+} /* namespace my */
