6f993b853c56c79777aa7243218ea063337270e7
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-11 13:12:14 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -60,8 +60,13 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
             {
                 _Ptr->refresh();
             }
-            std::cout << "std::shared_ptr::use_count " << _Elem.second.use_count() << std::endl;
+            std::cout << "std::shared_ptr::#1 " << _Elem.second.use_count() << std::endl;
         }
+        for (auto &_Elem : my::xmanager::instance().xobjects())
+        { 
+            std::cout << "std::shared_ptr::#2 " << _Elem.second.use_count() << std::endl;
+        }
+     
     }
     { // MISRA_CPP_00_01_07 함수 반환 값 미사용 금지
           std::int32_t _Key = 1;
