29f1a66ab19ced3a552c5f576434d8ecf511c0f7
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-11 10:51:40 +0900

    Update xmanager.cpp

--- src/xmanager.cpp
+++ src/xmanager.cpp
@@ -33,8 +33,8 @@ const std::map<int32_t, std::shared_ptr<xobject>>& xmanager::xobjects()
     return this->_Myxobjects;
 }
 
-// xobject *xmanager::insert( std::unique_ptr<xobject> _Uptr)
-xobject *xmanager::insert( std::shared_ptr<xobject> _Uptr)
+/*
+xobject *xmanager::insert( std::unique_ptr<xobject> _Uptr)
 {
     xobject *_Ptr = _Uptr.get();
     if (0==_Ptr->key())
@@ -49,7 +49,23 @@ xobject *xmanager::insert( std::shared_ptr<xobject> _Uptr)
     }
     return _Ptr;
 }
+*/
 
+std::weak_ptr<xobject> xmanager::insert( std::shared_ptr<xobject> _Uptr)
+{
+    std::weak_ptr<xobject> _Ptr = _Uptr;
+    if (0==_Ptr->key())
+    {
+        _Ptr->set_key( generate_unique_identifier() );
+        _Ptr->set_name( std::to_string(_Ptr->key()));
+    }
+    auto _Pair = this->_Myxobjects.insert(std::make_pair(_Uptr->key(), std::move(_Uptr)));
+    if (!_Pair.second) {
+        // error
+        _Ptr = nullptr;
+    }
+    return _Ptr;
+}
 void xmanager::erase( std::int32_t _Key)
 {
     if (0 == this->_Myxobjects.erase(_Key))
