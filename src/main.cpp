c3837f13f90921700ce8d8b1933fa613b7e15b83
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-11 11:18:15 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -50,17 +50,19 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
         }
         */
         
+        std::shared_ptr<my::xobject> _Ptr
         std::weak_ptr<my::xobject> _Wptr;
+     
         _Wptr = my::xmanager::instance().insert( std::make_shared<my::xmaneuver>() );
         // weak_ptr 를 shared_ptr 로 변환
-        std::shared_ptr<my::xobject> _Ptr = _Wptr.lock();  
+        _Ptr = _Wptr.lock();  
         if (nullptr != _Ptr)
         {
             // something
         }
         _Wptr = my::xmanager::instance().insert( std::make_shared<my::xsupport>() );
         // weak_ptr 를 shared_ptr 로 변환
-        std::shared_ptr<my::xobject> _Ptr = _Wptr.lock();  
+        _Ptr = _Wptr.lock();  
         if (nullptr != _Ptr)
         {
             // something
