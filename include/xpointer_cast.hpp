964c24efb0d7730eaca82381bc7b1e3af993e04d
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-11 23:18:14 +0900

    Update xpointer_cast.hpp

--- include/xpointer_cast.hpp
+++ include/xpointer_cast.hpp
@@ -1,11 +1,22 @@
+#ifndef XPOINTER_CAST_HPP
+#define XPOINTER_CAST_HPP
+
 namespace my {
 
+//static_pointer_cast overload for raw pointers
 template<class T, class U>
 inline T* static_pointer_cast(U *ptr)
-{ return static_cast<T*>(ptr); }
+{
+    return static_cast<T*>(ptr); 
+}
 
+//dynamic_pointer_cast overload for raw pointers
 template<class T, class U>
 inline T* dynamic_pointer_cast(U *ptr)
-{ return dynamic_cast<T*>(ptr); }
+{ 
+    return dynamic_cast<T*>(ptr);
+}
 
 } /* namespace my */
+
+#endif /* POINTER_CAST_HPP */
