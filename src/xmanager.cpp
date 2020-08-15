00699e3ad28ab0549f9bd47bb42e278e982ded14
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-15 20:47:06 +0900

    Update xmanager.cpp

--- src/xmanager.cpp
+++ src/xmanager.cpp
@@ -55,5 +55,16 @@ void xmanager::erase( std::int32_t _Key)
         // error
     }
 }
- 
+
+
+void assign_priority( std::int32_t _Key)
+{
+    my::xobject *_Vptr = xmanager::instance().find(_Key);
+    _Vptr->set_priority( xmanager::instance().generate_unique_identifier() );
+    if ( !_Vptr->submodels().empty() )
+    {
+        xmanager::instance().assign_priority_number(_Vptr);
+    }
+}
+  
 } /* namespace proj_devs */
