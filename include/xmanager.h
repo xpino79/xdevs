8a3bb92870817614ec3930bf96a6ff10227ce176
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-15 20:45:11 +0900

    Update xmanager.h

--- include/xmanager.h
+++ include/xmanager.h
@@ -67,10 +67,17 @@ public:
     
     
 };  
-
-// api
-xmanager* get_xmanager_ptr();
         
+void assign_priority( std::int32_t _Key)
+{
+    my::xobject *_Vptr = xmanager::instance().find(_Key);
+    _Vptr->set_priority( xmanager::instance().generate_unique_identifier() );
+    if ( !_Vptr->submodels().empty() )
+    {
+        xmanager::instance().assign_priority_number(_Vptr);
+    }
+}
+
 } /* namespace my */
 
 
