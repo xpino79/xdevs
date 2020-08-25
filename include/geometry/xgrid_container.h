96a5abce88f03fd880ed5c688957c3034c5c4362
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-25 11:48:36 +0900

    Update xgrid_container.h

--- include/geometry/xgrid_container.h
+++ include/geometry/xgrid_container.h
@@ -32,7 +32,7 @@ public:
     }
     ~xgrid()
     {
-        for (std::int32_t _Num : _Mymaximum_cols)
+        for (std::int32_t _Num : 20)
         {
             delete[] _Mytopography[_Num];
             _Mytopography[_Num] = nullptr;
@@ -50,8 +50,8 @@ public:
 class xgrid_container
 {
 private:
-    xcoordinate *_Myleft_bottom; 
-    xcoordinate *_Myright_top; 
+    std::unique_ptr<xcoordinate> _Myleft_bottom; 
+    std::unique_ptr<xcoordinate> _Myright_top; 
 
     xgrid **_Mygrid; 
     std::int32_t _Myinterval_xy; 
@@ -69,24 +69,56 @@ public:
         _Mymaximum_cols = 0;
         _Mymaximum_rows = 0;
     }
+    
     ~xgrid_container()
-    {
-        delete _Myleft_bottom;
-        delete _Myright_top;
-        
+    {       
         for (std::int32_t _Num : _Mymaximum_cols)
         {
             delete[] _Mygrid[_Num];
         }
         delete[] _Mygrid;
     }
+    
     void initialize(
         std::int32_t _Interval_xy, 
         std::int32_t _Left_bottom_x, std::int32_t _Left_bottom_y, 
         std::int32_t _Right_top_x, std::int32_t _Right_top_y
     )
     {
-    }
+        _Myinterval_xy = _Interval_xy;
+        _Myleft_bottom.set_x(_Left_bottom_x);
+        _Myleft_bottom.set_y(_Left_bottom_y);
+        _Myright_top.set_x(_Right_top_x);
+        _Myright_top.set_y(_Right_top_y);
+        _Mymaximum_cols = ((_Right_top_x - _Left_bottom_x) / _Interval_xy) + 1;
+        _Mymaximum_rows = ((_Right_top_y - _Left_bottom_y) / _Interval_xy) + 1;
+        
+        _Mygrid = new xgrid*[_Mymaximum_cols];
+        for (std::int32_t _Idx_x : _Mymaximum_cols)
+        {
+            _Mygrid[_Idx_x] = new xgrid[_Mymaximum_rows];
+            for (std::int32_t _Idx_y : _Mymaximum_rows)
+            {
+                _Mygrid[_Idx_x][_Idx_y].set_x(_Idx_x);
+                _Mygrid[_Idx_x][_Idx_y].set_y(_Idx_y);
+ 
+                if ((_Idx_x >= 0) && (_Idx_x < _Mymaximum_rows) &&
+                    (_Idx_y >= 0) && (_Idx_y < _Mymaximum_rows))
+                {
+                    _Mygrid[_Idx_x][_Idx_y]._Mytopography = new xtopography*[20];
+                    for (std::int32_t _Num : 20)
+                    {
+                        _Mygrid[_Idx_x][_Idx_y]._Mytopography[_Num] = new xtopography[20];
+                    }
+                }
+                else
+                {
+                    _Mygrid[_Idx_x][_Idx_y]._Mytopography = nullptr;
+                }
+
+            } // for
+        }  // for       
+    } 
     
 };
 
