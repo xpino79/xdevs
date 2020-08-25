6a40b51a507be448f57e8f13d798073b445fb147
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-25 11:55:16 +0900

    Create xgrid_manager.h

---
+++ include/xgrid_manager.h
@@ -0,0 +1,29 @@
+#ifndef XGRID_MANAGER_H_
+#define XGRID_MANAGER_H_
+
+#include "../include/geometry/xgrid_container.h"
+
+namespace my {
+
+class xgrid_manager
+{
+private:
+    xgrid_manager()
+    {
+    }
+    xgrid_manager(const xgrid_manager &) = delete; 
+    xgrid_manager &operator=(const xgrid_manager &) = delete; 
+    
+    static std::unique_ptr<xmanager> _Myinstance;
+    static std::once_flag _Myonce_flag;
+    
+    std::unique_ptr<xgrid_container> *_Mygrid_container;
+
+public:
+    static xgrid_manager& instance();
+    
+};
+
+} /* namespace my */
+
+#endif /* XGRID_MANAGER_H_ */
