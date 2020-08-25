c9f367f125aef49ff943afb05d8953b55080acfb
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-08-25 11:57:43 +0900

    Update and rename xmanager.cpp to xobject_manager.cpp

--- src/xobject_manager.cpp
+++ src/xobject_manager.cpp
@@ -2,48 +2,48 @@
  * Copyright 2020. xpino79@gmail.com All rights reserved.
  */
 
-#include "../include/xmanager.h"
+#include "../include/xobject_manager.h"
 
 namespace my
 {
-std::unique_ptr<xmanager> xmanager::_Myinstance;
-std::once_flag xmanager::_Myonce_flag;
-int32_t xmanager::_Myunique_identifier;
+std::unique_ptr<xobject_manager> xobject_manager::_Myinstance;
+std::once_flag xobject_manager::_Myonce_flag;
+int32_t xobject_manager::_Myunique_identifier;
 
 void _Myassign_priority( std::int32_t _Key)
 {
-    my::xobject *_Ptr = xmanager::instance().find(_Key);
-    _Ptr->set_priority( xmanager::instance().generate_unique_identifier() );
+    my::xobject *_Ptr = xobject_manager::instance().find(_Key);
+    _Ptr->set_priority( xobject_manager::instance().generate_unique_identifier() );
     if ( !_Ptr->submodels().empty() )
     {
-        // xmanager::instance().assign_priority_number( _Ptr );
+        // xobject_manager::instance().assign_priority_number( _Ptr );
         // >> 재귀 호출(recursive call)이란 함수 내부에서 함수가 자기 자신을 또다시 호출하는 행위를 의미합니다.
         std::for_each( _Ptr->submodels().begin(), _Ptr->submodels().end(), my::_Myassign_priority );
     }
 }
   
-xmanager::xmanager()
+xobject_manager::xobject_manager()
 {
 }
 
-xmanager& xmanager::instance()
+xobject_manager& xobject_manager::instance()
 {
     std::call_once(_Myonce_flag, []() {
-        _Myinstance.reset(new xmanager);
+        _Myinstance.reset(new xobject_manager);
     });
     return *(_Myinstance.get());
 }
 
-int32_t xmanager::generate_unique_identifier()
+int32_t xobject_manager::generate_unique_identifier()
 {
     return ++_Myunique_identifier;
 } 
-const std::map<int32_t, std::unique_ptr<xobject>>& xmanager::xobjects()
+const std::map<int32_t, std::unique_ptr<xobject>>& xobject_manager::xobjects()
 {
     return this->_Myxobjects;
 }
  
-xobject *xmanager::insert( std::unique_ptr<xobject> _Uptr)
+xobject *xobject_manager::insert( std::unique_ptr<xobject> _Uptr)
 {
     xobject *_Ptr = _Uptr.get();
     if (0==_Uptr->key())
@@ -58,9 +58,9 @@ xobject *xmanager::insert( std::unique_ptr<xobject> _Uptr)
     }
     return _Ptr;
 }
-void xmanager::erase( std::int32_t _Key)
+void xobject_manager::erase( std::int32_t _Key)
 {
-    std::cout << ">>>>> xmanager::erase " << std::endl;
+    std::cout << ">>>>> xobject_manager::erase " << std::endl;
     std::cout << " - key " << _Key << std::endl;
     if (0 == this->_Myxobjects.erase(_Key))
     {
