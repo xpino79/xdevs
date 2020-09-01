d60d2199246536f1eddbf0b25a21735132b4c92c
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-09-01 11:13:27 +0900

    Update xparallel.h

--- include/xparallel.h
+++ include/xparallel.h
@@ -13,10 +13,9 @@
 
 void _My_tbb_parallel_for()
 {
-    std::chrono::high_resolution_clock::time_point _Tbegin;
-    std::chrono::high_resolution_clock::time_point _Tend;
-    std::chrono::nanoseconds _Tnanoseconds;
-    std::chrono::seconds _Tseconds;
+    std::chrono::system_clock::time_point _Tbegin;
+    std::chrono::system_clock::time_point _Tend;
+    std::chrono::duration<double> _Tseconds;
     
     tbb::concurrent_unordered_map<int,int> _CUmap;
     for (int i=0;i < 900000;i++)
@@ -24,7 +23,7 @@ void _My_tbb_parallel_for()
         _CUmap.insert(tbb::concurrent_unordered_map<int,int>::value_type(i,i*10));
     }
 
-    _Tbegin = std::chrono::high_resolution_clock::now();
+    _Tbegin = std::chrono::system_clock::now();
     tbb::parallel_for(_CUmap.range(),[](const tbb::concurrent_unordered_map<int,int>::range_type& _Range)
     {
         for (auto _Iter = _Range.begin(); _Iter != _Range.end(); _Iter++)
@@ -33,10 +32,9 @@ void _My_tbb_parallel_for()
         }
     }, tbb::auto_partitioner() );
 
-    _Tend = std::chrono::high_resolution_clock::now();
-    _Tnanoseconds = _Tend-_Tbegin;
-    _Tseconds = std::chrono::duration_cast<std::chrono::seconds>(_Tnanoseconds);
-    std::cout << _Tseconds.count() << endl;
+    _Tend = std::chrono::system_clock::now();
+    _Tseconds = _Tend-_Tbegin;
+    std::cout << _Tseconds.count() << " sec" << std::endl;
 
     _Tbegin = std::chrono::high_resolution_clock::now();
     for (auto _Iter = _CUmap.begin(); _Iter != _CUmap.end(); _Iter++)
@@ -44,10 +42,9 @@ void _My_tbb_parallel_for()
         // printf("key : %d\t value : %d\n",_Iter->first,_Iter->second);
     }
 
-    _Tend = std::chrono::high_resolution_clock::now();
-    _Tnanoseconds = _Tend-_Tbegin;
-    _Tseconds = std::chrono::duration_cast<std::chrono::seconds>(_Tnanoseconds);
-    std::cout << _Tseconds.count() << endl;
+    _Tend = std::chrono::system_clock::now();
+    _Tseconds = _Tend-_Tbegin;
+    std::cout << _Tseconds.count() << " sec"<< std::endl;
  
 }
  
