f47c1ddb57b161f5d275c1fc1b42868720ce0a95
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-09-01 10:53:16 +0900

    _My_tbb_parallel_for()
    tbb-2018.2-9.el8.x86_64.rpm 
    tbb-devel-2018.2-9.el8.x86_64.rpm

--- src/main.cpp
+++ src/main.cpp
@@ -56,8 +56,7 @@ std::int32_t main(std::int32_t argc, std::char_t *argv[])
         my::xobject_manager::instance().assign_priority_number(_Pptr);
 
         // >>>>> std::map, std::vector 병렬처리 테스트
-        // _My_map_parallel();
-        // _My_vector_parallel( _Pptr );
+        // _My_tbb_parallel_for();
         
         return 1;
     }
