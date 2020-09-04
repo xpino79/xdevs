6dfc0fb8bf61bd18aa3fcbf42606a1db815ed603
woochan-lee <woochan.lee@aresinfo.com>
2020-09-04 17:35:43 +0900

    Add xpolygon::within

--- include/geometry/xpolygon.h
+++ include/geometry/xpolygon.h
@@ -4,23 +4,23 @@
 #ifndef XPOLYGON_H_
 #define XPOLYGON_H_
 
+#include <algorithm>
+#include <cmath>
 #include <memory>
 #include <mutex>
 #include <vector>
-#include <algorithm>
-#include <cmath>
 
-#include "xcoordinate.h"
 #include "../utility/stringtok.hpp"
+#include "xcoordinate.h"
 
 namespace my
 {
 class xpolygon
 {
-private:
+   private:
     std::vector<std::unique_ptr<xcoordinate>> _Mycoordinates;
-    
-public:
+
+   public:
     xpolygon() = default;
     ~xpolygon()
     {
@@ -29,7 +29,7 @@ public:
     void clear()
     {
         this->_Mycoordinates.clear();
-    }    
+    }
     bool empty()
     {
         return this->_Mycoordinates.empty();
@@ -38,7 +38,7 @@ public:
     {
         // WKT(Well-Known Text) Geometry
         // POLYGON((1 1,2 1,2 2,1 2,1 1))
-        
+
         /*
         auto _Pos = _Geometry.find("POLYGON");
         if ((_Pos != std::string::npos) && (_Pos == 0)) 
@@ -60,14 +60,13 @@ public:
             }            
         }
         */
-        
     }
-    
-    xcoordinate *get_coordinateN (std::size_t _Num)
+
+    xcoordinate *get_coordinateN(std::size_t _Num)
     {
         xcoordinate *_Ptr = nullptr;
         // Notice that the first element has a position of 0 (not 1).
-        if (this->_Mycoordinates.size() > _Num )
+        if (this->_Mycoordinates.size() > _Num)
         {
             _Ptr = this->_Mycoordinates.at(_Num).get();
         }
@@ -76,52 +75,52 @@ public:
 
     void push_back(std::int32_t _X, std::int32_t _Y)
     {
-        this->_Mycoordinates.push_back( std::make_unique<my::xcoordinate>(_X, _Y) );
+        this->_Mycoordinates.push_back(std::make_unique<my::xcoordinate>(_X, _Y));
     }
     void push_back(std::unique_ptr<xcoordinate> _Uptr)
     {
-        this->_Mycoordinates.push_back( std::move(_Uptr) );
+        this->_Mycoordinates.push_back(std::move(_Uptr));
     }
-    
+
     std::float64_t area()
     {
         std::float64_t _Val = 0.0;
         std::int32_t _Max = this->_Mycoordinates.size();
-        
+
         for (std::int32_t _Num = 0; _Num < _Max; ++_Num)
         {
             // _Num is the index of the first vertex, _Next is the next one.
             std::int32_t _Next = (_Num + 1) % _Max;
-            
+
             // The vertices of the edge we are checking.
             std::int32_t _X0 = this->_Mycoordinates[_Num]->x();
             std::int32_t _Y0 = this->_Mycoordinates[_Num]->y();
             std::int32_t _X1 = this->_Mycoordinates[_Next]->x();
             std::int32_t _Y1 = this->_Mycoordinates[_Next]->y();
-   
-            _Val += (0.5 * (_X0*_Y1 - _X1*_Y0));  
+
+            _Val += (0.5 * (_X0 * _Y1 - _X1 * _Y0));
         }
-        return _Val;        
+        return _Val;
     }
-    
+
     std::float64_t length()
     {
         std::float64_t _Val = 0.0;
         std::int32_t _Max = this->_Mycoordinates.size();
-        
+
         for (std::int32_t _Num = 0; _Num < _Max; ++_Num)
         {
             // _Num is the index of the first vertex, _Next is the next one.
             std::int32_t _Next = (_Num + 1) % _Max;
-            
+
             // The vertices of the edge we are checking.
             std::int32_t _X0 = this->_Mycoordinates[_Num]->x();
             std::int32_t _Y0 = this->_Mycoordinates[_Num]->y();
             std::int32_t _X1 = this->_Mycoordinates[_Next]->x();
             std::int32_t _Y1 = this->_Mycoordinates[_Next]->y();
-   
+
             _Val += (std::sqrt(std::pow(_X1 - _X0, 2) + std::pow(_Y1 - _Y0, 2)));
-        } 
+        }
         return _Val;
     }
 
@@ -132,12 +131,12 @@ public:
         // If we never cross any lines we're inside.
         bool _Inside = false;
         std::int32_t _Max = this->_Mycoordinates.size();
-    
+
         for (std::int32_t _Num = 0; _Num < _Max; ++_Num)
         {
             // _Num is the index of the first vertex, _Next is the next one.
             std::int32_t _Next = (_Num + 1) % _Max;
-            
+
             // The vertices of the edge we are checking.
             std::int32_t _X0 = this->_Mycoordinates[_Num]->x();
             std::int32_t _Y0 = this->_Mycoordinates[_Num]->y();
@@ -151,7 +150,7 @@ public:
                 // to a linear equation. Note that we can't get a division by zero here -
                 // if _Y1 == _Y0 then the above if be false.
                 std::float64_t _Cross = (_X1 - _X0) * (_Y - _Y0) / (_Y1 - _Y0) + _X0;
-                
+
                 // Finally check if it crosses to the left of our test point. You could equally
                 // do right and it should give the same result.
                 if (_Cross < _X)
@@ -160,12 +159,78 @@ public:
                 }
             }
         } /* for */
-        
+
         return _Inside;
     }
- 
+
+    bool within(std::int32_t _X, std::int32_t _Y, std::float64_t _Radius)
+    {
+        bool _Intersect = false;
+
+        _Intersect = within(_X, _Y);
+
+        if (false == _Intersect)
+        {
+            std::int32_t _Max = this->_Mycoordinates.size();
+
+            for (std::int32_t _Num = 0; _Num < _Max; ++_Num)
+            {
+                std::int32_t _Next = (_Num + 1) % _Max;
+
+                std::int32_t _X0 = this->_Mycoordinates[_Num]->x();
+                std::int32_t _Y0 = this->_Mycoordinates[_Num]->y();
+                std::int32_t _X1 = this->_Mycoordinates[_Next]->x();
+                std::int32_t _Y1 = this->_Mycoordinates[_Next]->y();
+
+                if (intersect(_X0, _Y0, _X1, _Y1, _X, _Y, _Radius))
+                {
+                    _Intersect = true;
+                    break;
+                }
+            } /* for */
+        }
+
+        return _Intersect;
+    }
+
+    bool intersect(std::float64_t _X0, std::float64_t _Y0, std::float64_t _X1, std::float64_t _Y1,
+                   std::float64_t _Circle_x, std::float64_t _Circle_y, std::float64_t _Radius)
+    {
+        bool _Retval = false;
+
+        // 원이 좌표의 중심에 있도록 이동합니다.
+        _X0 -= _Circle_x;
+        _Y0 -= _Circle_y;
+        _X1 -= _Circle_x;
+        _Y1 -= _Circle_y;
+
+        std::float64_t _Diff_x = _X1 - _X0;
+        std::float64_t _Diff_y = _Y1 - _Y0;
+
+        // 직선과 원의 교차점에 대한 이차 방정식의 계수를 구성합니다.
+        // 세그먼트 [0..1]에 음수 값이 있으면 세그먼트가 원과 교차합니다.
+        std::float64_t a = (_Diff_x * _Diff_x) + (_Diff_y * _Diff_y);
+        std::float64_t b = 2. * ((_X0 * _Diff_x) + (_Y0 * _Diff_y));
+        std::float64_t c = (_X0 * _X0) + (_Y0 * _Y0) - (_Radius * _Radius);
+
+        // 세그먼트 [0..1]에 솔루션이 있는지 확인하십시오.
+        if (-b < 0)
+        {
+            _Retval = (c < 0);
+        }
+        else if (-b < (2. * a))
+        {
+            _Retval = (((4. * a * c) - (b * b)) < 0);
+        }
+        else
+        {
+            _Retval = (a + b + c < 0);
+        }
+
+        return _Retval;
+    }
 };
-    
+
 } /* namespace my */
 
 #endif /* XPOLYGON_H_ */
