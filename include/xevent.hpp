fa36463256df55f2303f2904292462aa70983dd5
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-10-30 23:49:25 +0900

    Update xevent.hpp

--- include/xevent.hpp
+++ include/xevent.hpp
@@ -6,10 +6,10 @@ private:
     double _Myta = 0.0; //< 이벤트 시간
 public:
     xevent() = default;
-    ~xevent() = default;
+    virtual ~xevent() = default;
      
     diuble ta() { return _Myta; }
     set_ta(double _Val) { _Myta = _Val; }
     
-    void run() {}
+    virtual void run() {}
 };
