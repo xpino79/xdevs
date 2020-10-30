218fe5b925b43ff7600d6c5aa074e7e22f2c4574
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-10-31 00:00:57 +0900

    Update xpriority_queue.hpp

--- include/xpriority_queue.hpp
+++ include/xpriority_queue.hpp
@@ -1,7 +1,14 @@
 class xcompare
 {
+public:
+    bool operator()(const xevent *_Right, const xevent *_Left)
+    {
+        return _Right.ta() > _Left.ta();
+    }
+
 };
 
 class xpriority_queue
 {
+
 };
