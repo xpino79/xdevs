41adbab151cf747fbc9241b24b526a8c005187ba
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-10-31 00:10:17 +0900

    Update xevent_manager.hpp

--- include/xevent_manager.hpp
+++ include/xevent_manager.hpp
@@ -5,7 +5,11 @@ class xevent_manager
 {
 private:
     /// 싱글톤
+    xevent_manager() = default;
+
+    xpriority_queue _Myqueue;
 public:
+    
     /// 이벤트 등록 함수
     /// 이벤트 실행 함수
     /// 이벤트 취소 함수
