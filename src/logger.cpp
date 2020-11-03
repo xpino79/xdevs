1827968a4a1e34b7eac9d07c01b7bf2a445d3202
xpino79 <68993518+xpino79@users.noreply.github.com>
2020-11-03 18:45:19 +0900

    Update logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,41 +1,41 @@
 
 #include <iostream>
 
-#include "logger.h"
+#include "xlogger.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 
 namespace my {
 
 
-    std::shared_ptr<spdlog::logger> logger::_Mylogger;
-    std::once_flag logger::_Myonce_flag;
+std::shared_ptr<spdlog::logger> xlogger::_Mylogger;
+std::once_flag xlogger::_Myonce_flag;
 
-    /**
-     * @brief 인스턴스 반환
-     * @return
-     */
-    std::shared_ptr<spdlog::logger> logger::instance()
-    {
-        std::call_once(_Myonce_flag, []() {
-            spdlog::flush_every(std::chrono::seconds(1));
+/**
+ * @brief 인스턴스 반환
+ * @return
+ */
+std::shared_ptr<spdlog::logger> xlogger::instance()
+{
+    std::call_once(_Myonce_flag, []() {
+        spdlog::flush_every(std::chrono::seconds(1));
 
-            auto max_size = 1048576 * 5;
-            auto max_files = 3;
-            _Mylogger = spdlog::rotating_logger_mt("log", "logs/log", max_size, max_files, true);
-            _Mylogger->set_level(spdlog::level::trace);
+        auto max_size = 1048576 * 5;
+        auto max_files = 3;
+        _Mylogger = spdlog::rotating_logger_mt("log", "logs/log", max_size, max_files, true);
+        _Mylogger->set_level(spdlog::level::trace);
 
-            atexit(release_instance);
-        });
-        return _Mylogger;
-    }
-    /**
-     * @brief 인스턴스 초기화
-     * 
-     */
-    void logger::release_instance()
-    {
-        spdlog::shutdown();
-    }
+        atexit(release_instance);
+    });
+    return _Mylogger;
+}
+/**
+ * @brief 인스턴스 초기화
+ * 
+ */
+void xlogger::release_instance()
+{
+    spdlog::shutdown();
+}
 
 
 }
