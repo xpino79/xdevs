3edb771ef5a1c6f9c9b872cfb3f45d1898a8a0a3
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-01-13 19:40:42 +0900

    Update xparallel_stl.h

--- include/xparallel_stl.h
+++ include/xparallel_stl.h
@@ -1,2 +1,15 @@
 #include <execution>
 #include <algorithm>
+
+void _My_for_each()
+{
+  int num[] = {1, 2, 3};
+  std::vector a = { 4, 5, 6 };
+  std::mutex m;
+ 
+  std::for_each(std::execution::par, std::begin(num), std::end(num),
+    [&](auto v) {
+	std::lock_guard lock(m);
+	a.push_back(v + 1); // OK
+  });
+}
