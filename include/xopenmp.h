a896761df18db850d7a39fc62acd4692a7112e4c
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-01-21 11:31:28 +0900

    Update xopenmp.h

--- include/xopenmp.h
+++ include/xopenmp.h
@@ -4,10 +4,39 @@
 
 void _My_openmp_for()
 {
-  int sum = 0;
-  #pragma omp for schedule(static)
-  for(i=0; i<n; i++)
+  int _Sum = 0;
+
+  #pragma omp for schedule(static) reduction(+:_Sum)
+  for(int _Idx = 0; _Idx<10; ++_Idx)
   {
-    a[i] = work1(i);
+    _Sum += _Idx;
   }
+  
+  #pragma omp for schedule(static) collapse(2) reduction(+:_Sum)
+  for(int _Idx_x = 0; _Idx_x<10; ++_Idx_x)
+  {
+    for(int _Idx_y = 0; _Idx_y<10; ++_Idx_y)
+    {
+      _Sum += _Idx_y;
+    }
+  }
+
+  int _Max = 0;
+  #pragma omp for schedule(static) 
+  for(int _Idx = 0; _Idx<10; ++_Idx)
+  {
+    /* 교착상태 방지 */
+    if (_Idx > _Max)
+    {
+      #pragma omp critical
+      {
+        if (_Idx > _Max)
+        {
+          _Max = _Idx;
+          _Sum += _Idx;
+        }
+      }
+    }
+  }
+  
 }
