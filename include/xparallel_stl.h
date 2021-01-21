5dc35842c1037f397bae3841ce21e2c9ab63249b
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-01-21 13:03:52 +0900

    Update xparallel_stl.h

--- include/xparallel_stl.h
+++ include/xparallel_stl.h
@@ -12,22 +12,21 @@
 
 void _My_parallel_stl_for_each()
 {
-  int _Sum = 0;
-  std::mutex _Mutex;
- 
-  std::vector<int> _Vec = { 1, 2, 3 };
-  std::for_each(std::execution::par, std::begin(_Vec), std::end(_Vec),
-								[&](auto _Num) {
-									std::lock_guard _Guard(_Mutex);
-									Sum += _Num;
-								});
+    int _Sum = 0;
+    std::mutex _Mutex;
 
-  std::map<int, int> _Map{{1, 2}, {3, 4}, {5, 6}, {7, 8}};
-  std::for_each(std::execution::par, std::begin(_Map), std::end(_Map), 
-								[](const auto &_Pair) {
-									std::cout << "first " << _Pair.first << " second " << _Pair.second << std::endl;
-									std::lock_guard _Guard(_Mutex);
-									Sum += _Num;
-								});
+    std::vector<int> _Vec = {1, 2, 3};
+    std::for_each(std::execution::par, std::begin(_Vec), std::end(_Vec),
+                  [&](auto _Num) {
+                      std::lock_guard _Guard(_Mutex);
+                      Sum += _Num;
+                  });
 
+    std::map<int, int> _Map{{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+    std::for_each(std::execution::par, std::begin(_Map), std::end(_Map),
+                  [](const auto &_Pair) {
+                      std::cout << "first " << _Pair.first << " second " << _Pair.second << std::endl;
+                      std::lock_guard _Guard(_Mutex);
+                      Sum += _Num;
+                  });
 }
