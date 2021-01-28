0224a2c85895db61bdf912355788ef8d8d2fa6a5
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-01-28 09:15:06 +0900

    Update xcircle.h

--- include/geometry/xcircle.h
+++ include/geometry/xcircle.h
@@ -1,29 +1,29 @@
-
 /**
  * Copyright 2021. xpino79@gmail.com All rights reserved.
  *
- * @file xcircle.h
+ * @file coordinate.h
  * @date 2021. 01. 01.
  * @author xpino79@gmail.com
  * @brief 원 클래스 헤더파일
  */
 
-#ifndef XCIRCLE_H_
-#define XCIRCLE_H_
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
 
 #include <cstdint>
 #include <cmath>
+#include <limits>
 #include <iostream>
 
 namespace my
 {
     /**
-     * @class xcircle
+     * @class circle
      * @date 2021. 01. 01.
      * @author xpino79@gmail.com
      * @brief 원 클래스
      */
-    class xcircle
+    class circle
     {
     private:
         std::int32_t _Myx{};
@@ -31,11 +31,11 @@ namespace my
         double _Myr{};
 
     public:
-        explicit xcircle(std::int32_t _Pos_x, std::int32_t _Pos_y, double _Radius)
+        explicit circle(std::int32_t _Pos_x, std::int32_t _Pos_y, double _Radius)
             : _Myx(_Pos_x), _Myy(_Pos_y), _Myr(_Radius)
         {
         }
-        ~xcircle() = default;
+        ~circle() = default;
 
         std::int32_t x() const noexcept
         {
@@ -81,6 +81,33 @@ namespace my
             }
             return _Inside;
         }
+        bool intersects(std::int32_t _Begin_x, std::int32_t _Begin_y, std::int32_t _End_x, std::int32_t _End_y) const noexcept
+        {
+            double dx = _End_x - _Begin_x;
+            double dy = _End_y - _Begin_y;
+            double a = dx * dx + dy * dy;
+            double b = 2.0 * (dx * (_Begin_x - _Myx) + dy * (_Begin_y - _Myy));
+            double c = _Myx * _Myx + _Myy * _Myy;
+            c += _Begin_x * _Begin_x + _Begin_y * _Begin_y;
+            c -= 2.0 * (_Myx * _Begin_x + _Myy * _Begin_y);
+            c -= _Myr * _Myr;
+            double bb4ac = b * b - 4.0 * a * c;
+
+            if ((std::fabs(bb4ac) < std::numeric_limits<double>::epsilon()) || (bb4ac < 0))
+            {
+                return false; 
+            }
+            /*
+            double mu1 = (-b + std::sqrt(bb4ac)) / (2.0 * a);
+            double mu2 = (-b - std::sqrt(bb4ac)) / (2.0 * a); 
+            std::int32_t b1x = _Begin_x + mu1 * (_End_x - _Begin_x);
+            std::int32_t b1y = _Begin_y + mu1 * (_End_y - _Begin_y);
+            std::int32_t b2x = _Begin_x + mu2 * (_End_x - _Begin_x);
+            std::int32_t b2y = _Begin_y + mu2 * (_End_y - _Begin_y);   
+            std::cout << " > " << b1x << ", " << b1y << ", " << b2x << ", " << b2y << std::endl;    
+            */
+            return true;
+        }
         void print() const
         {
             std::cout << _Myx << " " << _Myy << " " << _Myr << std::endl;
@@ -89,4 +116,4 @@ namespace my
 
 } /* namespace my */
 
-#endif /* XCIRCLE_H_ */
+#endif /* CIRCLE_H_ */
