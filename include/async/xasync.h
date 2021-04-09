3ef237332402f6ef6fedd2a4bac734c2466697fe
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-04-09 23:26:42 +0900

    Update xasync.h

--- include/async/xasync.h
+++ include/async/xasync.h
@@ -13,25 +13,62 @@ void _My_for_print(int num) {
     printf("%d번 Thread : %d\n", num, i);
 }
 
-  // 분산작엄 예제
+  // 분산작업 예제
 void _My_async()
 {
-  std::vector<std::future<void>> v_async;
-  v_async.reserve(2000);
+  std::vector<std::future<void>> futures;
+  futures.reserve(2000);
 
   // async는 future로 받지 않을 경우 sequential이라 생각하고 순서대로 진행이 된다.
   for (size_t i = 0; i < 2000; i++)
   {
     // launch::async 옵션을 주면 바로 그 자리에서 쓰레드를 생성해서 실행
     // launch::deferred 옵션을 주면, future 의 get 을 하였을 때 비로소 (동기적으로) 실행
-    v_async.emplace_back(std::async(std::launch::async, _My_for_print, i));
+    futures.emplace_back(std::async(std::launch::async, _My_for_print, i));
   }
   for (size_t i = 0; i < 2000; i++)
   {
-    v_async[i].wait();
+    futures[i].wait();
   }
 }
 
+/*
+void _My_async_1()
+{
+  bool retval = true;
+  
+  // original
+  for(auto &elem : jobs)
+  {
+    if (false == elem.execute())
+    {
+      retval = false;
+    }
+  }
+  
+  // distribution
+  std::vector<std::future<bool>> futures;
+  for(auto &elem : jobs)
+  {
+    futures.push_back(
+      std::async(
+        std::launch::async, 
+        [&]
+        {
+          elem.execute();
+        }
+      ));
+  }
+  for(const auto & elem : futures)
+  {
+    if (false == elem.get())
+    {
+      retval = false;
+    }
+  }
+}
+*/
+  
 } /* namespace my */
 
 #endif /* XGRID_MANAGER_H_ */
