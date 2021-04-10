03acc114cba8d20e78f69553fa0325f16b6f7884
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-04-10 12:26:14 +0900

    Update xgprof.hpp

--- include/gprof/xgprof.hpp
+++ include/gprof/xgprof.hpp
@@ -7,3 +7,6 @@
 // g++ 2d_percolation.c mt19937ar.c -pg -o example.out
 // chmod 755 example.out
 // gprof example.out > gprof.log
+
+// https://m.blog.naver.com/hermet/175053298
+// https://blog.naver.com/hermet/111117366
