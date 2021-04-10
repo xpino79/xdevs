f0fd2940e4fd0f761277dcdfaf58861c598e32d0
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-04-10 12:19:28 +0900

    Update xgprof.hpp

--- include/gprof/xgprof.hpp
+++ include/gprof/xgprof.hpp
@@ -1,1 +1,9 @@
-gprof
+// https://noanswercode.tistory.com/2
+// https://noanswercode.tistory.com/51
+
+// 설치
+// sudo apt-get install binutils
+
+// g++ 2d_percolation.c mt19937ar.c -pg -o example.out
+// chmod 755 example.out
+// gprof example.out > gprof.log
