7494a2bcbb3ee16607951e9d247dc6b4605d7c2f
xpino79 <68993518+xpino79@users.noreply.github.com>
2021-12-09 13:17:17 +0900

    Update xasync.h

--- include/async/xasync.h
+++ include/async/xasync.h
@@ -1,77 +1,52 @@
-#ifndef XASYNC_H_
-#define XASYNC_H_
-
-namespace my {
-  
-#include <iostream>
-#include <vector>
+#include <thread>
 #include <future>
-#include <cstdio>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
 
-// https://www.slideshare.net/SangHeonLee9/ndc2016-tera-modern-c
-// https://www.slideshare.net/sangkikim9/ndc2016-64837136
-  
-void _My_for_print(int num) {
-  for (int i = 0; i < 100; i++)
-    printf("%d번 Thread : %d\n", num, i);
-}
-
-  // 분산작업 예제
-void _My_async()
+bool __compar(std::size_t _x, std::size_t _j)
 {
-  std::vector<std::future<void>> futures;
-  futures.reserve(2000);
-
-  // async는 future로 받지 않을 경우 sequential이라 생각하고 순서대로 진행이 된다.
-  for (size_t i = 0; i < 2000; i++)
-  {
-    // launch::async 옵션을 주면 바로 그 자리에서 쓰레드를 생성해서 실행
-    // launch::deferred 옵션을 주면, future 의 get 을 하였을 때 비로소 (동기적으로) 실행
-    futures.emplace_back(std::async(std::launch::async, _My_for_print, i));
-  }
-  for (size_t i = 0; i < 2000; i++)
-  {
-    futures[i].wait();
-  }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return (_x < _j);
 }
 
-/*
-void _My_async_1()
+std::int32_t main()
 {
-  bool retval = true;
-  
-  // original
-  for(auto &elem : jobs)
-  {
-    if (false == elem.execute())
-    {
-      retval = false;
-    }
-  }
-  
-  // distribution
-  std::vector<std::future<bool>> futures;
-  for(auto &elem : jobs)
-  {
-    futures.push_back(
-      std::async(
-        std::launch::async, 
-        [&]
-        {
-          elem.execute();
-        }
-      ));
-  }
-  for(const auto & elem : futures)
-  {
-    if (false == elem.get())
-    {
-      retval = false;
-    }
-  }
-}
-*/
-  
-} /* namespace my */
+    std::cout << "run" << std::endl;
+    auto _begin = std::chrono::system_clock::now();
 
-#endif /* XGRID_MANAGER_H_ */
+    // std::vector<std::future<bool>> _futures;
+    std::unordered_map<std::size_t, std::future<bool>> _futures;
+    std::size_t _x = 1;
+    for (std::size_t _i = 0; _i < 1000; ++_i)
+    {
+        // _futures.push_back(std::async(
+        _futures.emplace(_i, std::async(
+                                 std::launch::async,
+                                 [](std::size_t _x, std::size_t _i)
+                                 {
+                                     return __compar(_x, _i);
+                                 },
+                                 _x, _i));
+    }
+
+    for (auto &_future : _futures)
+    {
+        // std::cout << " >>> " << _future.get() << std::endl;
+        // _future.get();
+
+        std::cout << " >>> " << _future.first << ", " << _future.second.get() << std::endl;
+        // _future.first;
+        // _future.second.get();
+    }
+
+    auto _end = std::chrono::system_clock::now();
+    // std::chrono::microseconds _microseconds = std::chrono::duration_cast<std::chrono::microseconds>(_end - _begin);
+    // std::chrono::milliseconds _milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(_end - _begin);
+    std::chrono::seconds _seconds = std::chrono::duration_cast<std::chrono::seconds>(_end - _begin);
+    // std::chrono::minutes _minutes = std::chrono::duration_cast<std::chrono::minutes>(_end - _begin);
+    // std::chrono::hours _hours = std::chrono::duration_cast<std::chrono::hours>(_end - _begin);
+
+    std::cout << "end: " << _seconds.count() << std::endl;
+    return EXIT_SUCCESS;
+}
